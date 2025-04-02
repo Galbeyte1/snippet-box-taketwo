610ecbe951bc05505dd067f3953a2b3fef513d92
Galbeyte <s.galbeyte@gmail.com>
2025-04-02 15:10:38 -0500

    2.3 Routing requests: registered new routes. Interesting usage for making multiple pages for an application. Used {$} to limit routes ending with / from being a catchall. Learned explicit usage of mux has some security benefits. Refer back to this section for more info.

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet... "))
+}
+
+func snippetWrite(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for writing a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/write", snippetWrite)
 
 	log.Print("starting server on :4000")
 
