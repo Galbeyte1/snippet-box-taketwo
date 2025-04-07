58cd06612257a53152c5e161a294d36ee0a8c3f7
Galbeyte <s.galbeyte@gmail.com>
2025-04-07 13:36:21 -0500

    5.6 Custom template functions: demonstrated use of custom template functions by creating a func map that currently allows for the formating of time to human readable date. Maybe sometime a hijri calendar can be used.

--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -15,6 +15,15 @@ type TemplateData struct {
 	Snippets    []models.Snippet
 }
 
+func humanDate(t time.Time) string {
+
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func NewTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
@@ -26,7 +35,7 @@ func NewTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
